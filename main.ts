f9251650805b1728f348b55513573e5ac389884d
mguelcan <93864445+mguelcan@users.noreply.github.com>
2026-05-28 09:42:12 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+if (true) {
+	
+}
+basic.forever(function () {
+    wuKong.setServoAngle(wuKong.ServoTypeList._180, wuKong.ServoList.S0, 0)
+    basic.pause(1000)
+    wuKong.setServoAngle(wuKong.ServoTypeList._180, wuKong.ServoList.S0, 100)
+    basic.pause(1000)
+})
